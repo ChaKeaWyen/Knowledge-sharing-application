ff0933d9a135d53b6d0565090fcbedd9253e904e
Jeen <jeen.nan.pi.jeen12@gmail.com>
2021-12-10 17:24:17 +0100

    import for new login + add some extra code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore: unused_import
 import 'package:almost/screen/Home.dart';
+import 'package:almost/screen_rewrite/constant.dart';
+import 'package:almost/screen_rewrite/new_login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "My App",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Bgcolor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: HomeScreen(),
+      home: Loginre(),
     );
   }
 }
