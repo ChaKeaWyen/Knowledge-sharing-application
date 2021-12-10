2e16efc9f83b04441df19c03ce9f39b42016a301
Jeen <jeen.nan.pi.jeen12@gmail.com>
2021-12-10 17:25:41 +0100

    New code: Defining color constant (our theme color)

---
+++ lib/screen_rewrite/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Bgcolor = Color(0xffedd9d4);
+const lemon = Color(0xffead095);
+const Peach = Color(0xffed8568);
+const Wine = Color(0xffc14b3d);
+const Matcha = Color(0xff45533c);
+const Grass = Color(0xff8a9d70);
