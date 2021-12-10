2293c234d45f96e9da07206b4cf9c93270218b71
Jeen <jeen.nan.pi.jeen12@gmail.com>
2021-12-10 17:26:47 +0100

    Rewrite all Login (no use of UI) : Prototype1

---
+++ lib/screen_rewrite/new_login.dart
@@ -0,0 +1,11 @@
+import 'package:almost/screen_rewrite/components/body.dart';
+import 'package:flutter/material.dart';
+
+class Loginre extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
