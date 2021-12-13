b05c25aa8b7068ba22d037f7c33af3378f82c3e2
ChaKeaWyen <2547ninemoon@gmail.com>
2021-12-14 01:39:05 +0700

    SignUp1st step

---
+++ lib/SignUp_rewrite/SignUp_screen.dart
@@ -0,0 +1,13 @@
+import 'package:almost/xdregiste_r.dart';
+import 'package:flutter/material.dart';
+
+class SignUpre extends StatelessWidget {
+  const SignUpre({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: XDREGISTER(),
+    );
+  }
+}
