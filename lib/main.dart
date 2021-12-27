86ed35e5eb08e0cfca040265b43f540873031f5d
Jeen <jeen.nan.pi.jeen12@gmail.com>
2021-12-28 00:05:21 +0100

    change bg and test

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 //import 'package:almost/screen/Home.dart';
 // ignore: unused_import
+import 'package:almost/Pages/homepages.dart';
 import 'package:almost/SignUp_rewrite/SignUp_screen.dart';
 import 'package:almost/constant.dart';
 // ignore: unused_import
 import 'package:almost/login_rewrite/new_login.dart';
+import 'package:almost/screen/home.dart';
 // ignore: unused_import
 import 'package:almost/screen/register.dart';
 
@@ -31,10 +33,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "My App",
       theme: ThemeData(
-        primaryColor: Bgcolor,
-        scaffoldBackgroundColor: Color(0xffedd9d4),
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Color(0000000000),
       ),
-      home: RegisterScreen(),
+      home: Homere(),
     );
   }
 }
