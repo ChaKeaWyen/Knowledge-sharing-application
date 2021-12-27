0ebb20ee1b73a64bbccd6f4151cec6c7e3cadd21
Jeen <jeen.nan.pi.jeen12@gmail.com>
2021-12-28 00:05:55 +0100

    exclude firebase code

--- lib/components/roundedbutton.dart
+++ lib/components/roundedbutton.dart
@@ -13,7 +13,7 @@ class RoundedButton extends StatelessWidget {
   Profile profile = Profile(email: '', password: '');
   final Future<FirebaseApp> firebase = Firebase.initializeApp();
   final String text;
-  final Function press;
+  final press;
   final Color color, textColor;
   RoundedButton({
     Key? key,
@@ -30,33 +30,34 @@ class RoundedButton extends StatelessWidget {
       margin: EdgeInsets.symmetric(vertical: 50),
       width: size.width * 0.7,
       child: TextButton(
-        child: Text(text),
-        style: TextButton.styleFrom(
-            primary: textColor,
-            backgroundColor: Colors.grey[700],
-            shape: const BeveledRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            textStyle: TextStyle(
-              fontSize: 20,
-            )),
-        onPressed: () async {
-          if (formKey.currentState!.validate()) {
-            formKey.currentState!.save();
-            try {
-              await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: profile.email, password: profile.password);
-              formKey.currentState!.reset();
-            } on FirebaseAuthException catch (e) {
-              print(e.message);
-            }
-          }
-          Navigator.push(context, MaterialPageRoute(
-            builder: (context) {
-              return Loginre();
-            },
-          ));
-        },
-      ),
+          child: Text(text),
+          style: TextButton.styleFrom(
+              primary: textColor,
+              backgroundColor: Colors.grey[700],
+              shape: const BeveledRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              textStyle: TextStyle(
+                fontSize: 20,
+              )),
+          onPressed: press
+          //() async {
+          //if (formKey.currentState!.validate()) {
+          // formKey.currentState!.save();
+          // try {
+          //   await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          //       email: profile.email, password: profile.password);
+          //   formKey.currentState!.reset();
+          // } on FirebaseAuthException catch (e) {
+          //   print(e.message);
+          // }
+          //}
+          //Navigator.push(context, MaterialPageRoute(
+          // builder: (context) {
+          //  return Loginre();
+          //},
+          //));
+          //},
+          ),
     );
   }
 }
